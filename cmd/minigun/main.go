17371d3425822f7b827cf51a8426f90910727e7c
Alex <naplle58@gmail.com>
2023-10-29 18:42:15 +0300

    add file open on args

--- cmd/minigun/main.go
+++ cmd/minigun/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/ravsii/minigun/internal/command"
 	"github.com/ravsii/minigun/internal/keybind"
@@ -14,6 +16,11 @@ func main() {
 	mg := minigun.New()
 	ch := command.New(&mg)
 	kh := keybind.New(&ch)
+
+	if len(os.Args) > 1 {
+		ch.OpenFile(os.Args[1:]...)
+	}
+
 	mg.Draw()
 
 	for {
